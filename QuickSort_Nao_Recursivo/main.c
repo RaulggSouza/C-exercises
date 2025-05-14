850c9bb2076038295dd959aa1976f59de38afd61
RaulggSouza <raulggsouza@hotmail.com>
2025-05-14 13:58:38 -0300

    starting quick sort with stack

---
+++ QuickSort_Nao_Recursivo/main.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+
+typedef struct t_no{
+    struct t_no* next;
+    struct t_no* prev;
+    int start_index;
+    int end_index;
+} t_node;
+
+typedef struct {
+    t_node* start;
+    t_node* top;
+}t_stack;
+
+t_stack* create_stack(){
+    t_stack* stack = (t_stack*) malloc(sizeof(t_stack));
+    stack->start = NULL;
+    stack->top = NULL;
+    return stack;
+}
+
+t_node* create_node(int start, int end){
+    t_node* node = (t_node*) malloc(sizeof(t_node));
+    node->start_index = start;
+    node->end_index = end;
+    node->next = NULL;
+    node->prev = NULL;
+    return node;
+}
+
+void insert(t_stack* stack, t_node* node){
+    if(stack->start == NULL){
+        stack->start = node;
+        stack->top = node;
+    } else {
+        
+    }
+}
+
+void swap(int* vet, int start, int end, int* pivot){
+    int i = start, j = end, aux;
+    while (j >= i){
+        if (vet[i] > vet[*pivot]){
+            if (vet[j] <= vet[*pivot]){
+                aux = vet[i];
+                vet[i] = vet[j];
+                vet[j] = aux;
+                if (vet[j] == vet[*pivot]) {
+                    *pivot = i;
+                } 
+            }
+            j--;
+        } else {
+            i++;
+        }
+    }
+}
+
+void quick(int* vet, int start, int len){
+    if (len==start){
+        return;
+    }
+    int pivot = (len+start)/2;
+    swap(vet, start, len-1, &pivot);
+    printf("%d - %d - %d\n", start, len, pivot);
+    quick(vet, start, pivot);
+    quick(vet, pivot+1, len);
+}
+
+int main(int argc, char const *argv[]){
+    //  int vector[] = {1,2,3,3,1,2,4,3};
+    // int vector[] = {3,2,2,2,3,4,4,4};
+    int vector[] = {8,7,6,5,4,3,2,1};
+    for (int i = 0; i < 8; i++){
+        printf("%d\t", vector[i]);
+    }
+    printf("\n");
+    quick(vector, 0, 8);
+    for (int i = 0; i < 8; i++){
+        printf("%d\t", vector[i]);
+    }
+    return 0;
+}
